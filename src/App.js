ce8580c20cffe5b92a6de342e5811fe11f6e2c97
Harry Tucker <harrytuckerubuntu@gmail.com>
2019-09-26 11:21:04 +0100

    Add BidTable component and Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap-dist/css/bootstrap.min.css';
+import Table from 'react-bootstrap/Table'
+
+class BidTable extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount() {
+    fetch('http://0.0.0.0:61125/api/v1/bids/21')
+      .then(results => {
+        return results.json()
+      }).then(data => {
+        let bids = data.map((bid) => {
+          return (
+            <tr>
+              <td>{bid.bidder_name}</td>
+              <td>£{bid.bid_amount}</td>
+            </tr>
+          )
+        })
+
+        this.setState({ bids: bids })
+        console.log("state", this.state.bids)
+      })
+  }
+
+  render() {
+    return (
+      <Table striped bordered variant="dark">
+        <thead>
+          <td>Bidder</td>
+          <td>Bid Amount</td>
+        </thead>
+        <tbody>
+          {this.state.bids}
+        </tbody>
+      </Table>
+    )
+  }
+}
 
 function App() {
   return (
@@ -18,6 +60,9 @@ function App() {
         >
           Learn React
         </a>
+        <div style={{ width: '40%' }}>
+          <BidTable></BidTable>
+        </div>
       </header>
     </div>
   );
