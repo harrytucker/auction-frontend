fbfa8cc3469dd54dd3aa7f2479b696b14b392c55
Harry Tucker <harrytuckerubuntu@gmail.com>
2019-09-26 20:10:58 +0100

    Code sundries

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import './bootstrap-dist/css/bootstrap.min.css';
 import Table from 'react-bootstrap/Table'
-import Badge from 'react-bootstrap/Badge'
 import Button from 'react-bootstrap/Button'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
-import { from } from 'rxjs';
+import Badge from 'react-bootstrap/Badge'
 
 class BidTable extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { value: 1 }
+    this.state = { value: null }
 
     this.handleChange = this.handleChange.bind(this)
-  }
-
-  componentDidMount() {
-    fetch('http://0.0.0.0:61125/api/v1/bids/1')
-      .then(results => {
-        return results.json()
-      }).then(data => {
-        let bids = data.map((bid) => {
-          return (
-            <tr>
-              <td>{bid.bidder_name}</td>
-              <td>£{bid.bid_amount}</td>
-            </tr>
-          )
-        })
-
-        this.setState({ bids: bids })
-        console.log("state", this.state.bids)
-      })
   }
 
   handleChange(event) {
@@ -67,7 +46,7 @@ class BidTable extends React.Component {
       <div>
         <Form>
           <Form.Label>Item Number</Form.Label>
-          <Form.Control onChange={this.handleChange} name="value" value={this.state.value} type="number" placeholder="My Name" required="true"></Form.Control>
+          <Form.Control onChange={this.handleChange} name="value" value={this.state.value} type="number" min="1" placeholder="Item Number From Spreadsheet" required="true"></Form.Control>
           <Form.Text className="text-muted">View bids for an item</Form.Text>
         </Form>
         <Table striped bordered hover>
@@ -89,7 +68,7 @@ class BidTable extends React.Component {
 class BidForm extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { bidder_name: '', bidder_email: '', item_number: 1, bid_amount: 1 }
+    this.state = { bidder_name: '', bidder_email: '', item_number: 1, bid_amount: 1, success: false }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -124,6 +103,8 @@ class BidForm extends React.Component {
       return results.json()
     }).then(data => {
       console.log(data)
+      // show success badge
+      this.setState({ success: true })
     })
 
     event.preventDefault()
@@ -148,7 +129,7 @@ class BidForm extends React.Component {
 
         <Form.Group controlId="item_number">
           <Form.Label>Item Number</Form.Label>
-          <Form.Control onChange={this.handleChange} name="item_number" type="number" placeholder="1" min="1" required="true"></Form.Control>
+          <Form.Control onChange={this.handleChange} name="item_number" type="number" placeholder="Item Number From Spreadsheet" min="1" required="true"></Form.Control>
           <Form.Text className="text-muted">Please refer to the excel spreadsheet for items available</Form.Text>
         </Form.Group>
 
@@ -164,6 +145,10 @@ class BidForm extends React.Component {
             Submit
           </Button>
         </Form.Group>
+        <Form.Group>
+          <Badge hidden={!this.state.success} variant="success">Bid Placed!</Badge>
+          <Form.Text hidden={!this.state.success}>Please refresh the page to show your bid in the table to the left</Form.Text>
+        </Form.Group>
       </Form>
     )
   }
@@ -173,7 +158,7 @@ function App() {
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark" fixed="top">
-        <Navbar.Brand href="#">
+        <Navbar.Brand href="/">
           <img
             alt=""
             src="/hpe-light.svg"
